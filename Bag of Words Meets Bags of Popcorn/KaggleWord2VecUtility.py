aa0ab928548f306310778781cfbe46d7e2da618b
Claus Lv <claus.lv@hotmail.com>
2015-11-30 21:15:08 +0800

    Try xgboost for multi class classification

--- Bag of Words Meets Bags of Popcorn/KaggleWord2VecUtility.py
+++ Bag of Words Meets Bags of Popcorn/KaggleWord2VecUtility.py
@@ -12,7 +12,7 @@ class KaggleWord2VecUtility(object):
     """KaggleWord2VecUtility is a utility class for processing raw HTML text into segments for further learning"""
 
     @staticmethod
-    def review_to_wordlist( review, remove_stopwords=False ):
+    def review_to_wordlist(review, remove_stopwords=False):
         # Function to convert a document to a sequence of words,
         # optionally removing stop words.  Returns a list of words.
         #
@@ -20,7 +20,7 @@ class KaggleWord2VecUtility(object):
         review_text = BeautifulSoup(review).get_text()
         #
         # 2. Remove non-letters
-        review_text = re.sub("[^a-zA-Z]"," ", review_text)
+        review_text = re.sub("[^a-zA-Z]", " ", review_text)
         #
         # 3. Convert words to lower case and split them
         words = review_text.lower().split()
@@ -35,7 +35,7 @@ class KaggleWord2VecUtility(object):
 
     # Define a function to split a review into parsed sentences
     @staticmethod
-    def review_to_sentences( review, tokenizer, remove_stopwords=False ):
+    def review_to_sentences(review, tokenizer, remove_stopwords=False):
         # Function to split a review into parsed sentences. Returns a
         # list of sentences, where each sentence is a list of words
         #
@@ -48,8 +48,8 @@ class KaggleWord2VecUtility(object):
             # If a sentence is empty, skip it
             if len(raw_sentence) > 0:
                 # Otherwise, call review_to_wordlist to get a list of words
-                sentences.append( KaggleWord2VecUtility.review_to_wordlist( raw_sentence, \
-                  remove_stopwords ))
+                sentences.append(KaggleWord2VecUtility.review_to_wordlist(
+                    raw_sentence, remove_stopwords))
         #
         # Return the list of sentences (each sentence is a list of words,
         # so this returns a list of lists
